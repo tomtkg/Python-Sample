43a230ba84a888ce5ce69842b6b70c8d69944b9a
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2025-01-26 09:42:21 +0900

    Create rank_d.py

---
+++ 電脳少女プログラミング2088/rank_d.py
@@ -0,0 +1,10 @@
+def alley():
+    print(max(int(input()) for _ in range(int(input()))))
+
+
+def casino():
+    print(sum(i * int(input()) for i in [1, 5, 10]))
+
+
+def slums():
+    print(int(int(input()) / 2 + 100))
