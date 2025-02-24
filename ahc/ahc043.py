742ab99a54e09dbf9eabbcf34eaaf6a3edeff9c7
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2025-02-24 20:13:41 +0900

    Update ahc043.py

--- ahc/ahc043.py
+++ ahc/ahc043.py
@@ -70,12 +70,17 @@ def build_candidate(
     return ext_pos
 
 
-def get_ids(stations: set[Pos], map: dict[Pos, set[int]]) -> set[int]:
-    ids = set()
-    for p in stations:
-        if p in map:
-            ids |= map[p]
-    return ids
+def get_ids(
+    pos_to_idx: dict[Pos, tuple[set[int], set[int]]], poss: list[Pos]
+) -> tuple[set[int], set[int]]:
+    set1, set2 = set(), set()
+    for pos in poss:
+        if pos in pos_to_idx:
+            idx1, idx2 = pos_to_idx[pos]
+            set1.update(idx1)
+            set2.update(idx2)
+
+    return set1, set2
 
 
 def is_between(p1: Pos, p2: Pos, p: Pos):
@@ -84,7 +89,7 @@ def is_between(p1: Pos, p2: Pos, p: Pos):
     )
 
 
-def next_position(flag: bool, p: Pos, target: Pos) -> Pos:
+def next_pos(flag: bool, p: Pos, target: Pos) -> Pos:
     r, c = p
     tr, tc = target
     if (flag and c != tc) or (not flag and r == tr):
@@ -132,7 +137,6 @@ class Solver:
         T: int,
         idx_to_pos: list[tuple[Pos, Pos]],
         pos_to_idx: dict[Pos, tuple[set[int], set[int]]],
-        candidate: set[Pos],
     ):
         self.N = N
         # self.M = M
@@ -140,16 +144,18 @@ class Solver:
         self.T = T
         self.idx_to_pos = idx_to_pos
         self.pos_to_idx = pos_to_idx
-        self.candidate = candidate
 
         self.money = K
         self.income = 0
         self.actions = []
+        self.rails = set()
+        self.stations = set()  # 建設済みの駅
+        self.stations2 = set()  # 建設済みの駅 + 線路上の駅候補
 
-    def calc_income(self, stations: set[Pos]) -> int:
-        home_ids = get_ids(stations, self.home_map)
-        work_ids = get_ids(stations, self.work_map)
-        return sum(distance(self.home[id], self.work[id]) for id in home_ids & work_ids)
+    def calc_income(self, stations: set[Pos] = {()}) -> int:
+        stations |= self.stations
+        home_ids, work_ids = get_ids(self.pos_to_idx, stations)
+        return sum(distance(*self.idx_to_pos[idx]) for idx in home_ids & work_ids)
 
     def build_rail(self, type: int, r: int, c: int) -> None:
         while self.money < COST_RAIL:
@@ -158,17 +164,21 @@ class Solver:
         self.actions.append(f"{type} {r} {c}")
 
     def build_rails(self, path: list[Pos]) -> None:
-        for p in range(1, len(path) - 1):
-            symbol = get_symbol(path[p - 1], path[p], path[p + 1])
-            self.build_rail(symbol, path[p][0], path[p][1])
+        self.rails.update(path)
+        for i in range(1, len(path) - 1):
+            symbol = get_symbol(path[i - 1], path[i], path[i + 1])
+            self.build_rail(symbol, path[i][0], path[i][1])
 
-    def build_station(self, r: int, c: int) -> None:
-        self.stations.add((r, c))
+    def build_station(self, p: Pos) -> None:
+        if p in self.stations:
+            return
         while self.money < COST_STATION:
             self.build_nothing()
-        self.income = self.calc_income(self.stations)
+
+        self.stations.add(p)
+        self.income = self.calc_income()
         self.money += self.income - COST_STATION
-        self.actions.append(f"{0} {r} {c}")
+        self.actions.append(f"{0} {p[0]} {p[1]}")
 
     def build_nothing(self) -> None:
         self.money += self.income
@@ -198,63 +208,49 @@ class Solver:
 
         return st, st2
 
-    def solve(self) -> None:
-        # 接続する人を見つける
-        rail_count = (self.K - COST_STATION * 2) // COST_RAIL
-        person_idx = 0
-        while person_idx < self.M:
-            if distance(self.home[person_idx], self.workplace[person_idx]) - 1 <= rail_count:
-                break
-            person_idx += 1
-        assert person_idx != self.M
+    def solve(self, candidate: set[Pos]):
+        # 建設候補駅を見つける
+        stations, candidate = self.find_2stations(candidate)
+        while len(candidate) > 0:
+            stations, candidate = self.find_next(stations, candidate)
 
-        # 駅の配置
-        self.build_station(*self.home[person_idx])
-        self.build_station(*self.workplace[person_idx])
-
-        # 線路を配置して駅を接続する
-        r0, c0 = self.home[person_idx]
-        r1, c1 = self.workplace[person_idx]
-        # r0 -> r1
-        if r0 < r1:
-            for r in range(r0 + 1, r1):
-                self.build_rail(RAIL_VERTICAL, r, c0)
-            if c0 < c1:
-                self.build_rail(RAIL_RIGHT_UP, r1, c0)
-            elif c0 > c1:
-                self.build_rail(RAIL_LEFT_UP, r1, c0)
-        elif r0 > r1:
-            for r in range(r0 - 1, r1, -1):
-                self.build_rail(RAIL_VERTICAL, r, c0)
-            if c0 < c1:
-                self.build_rail(RAIL_RIGHT_DOWN, r1, c0)
-            elif c0 > c1:
-                self.build_rail(RAIL_LEFT_DOWN, r1, c0)
-        # c0 -> c1
-        if c0 < c1:
-            for c in range(c0 + 1, c1):
-                self.build_rail(RAIL_HORIZONTAL, r1, c)
-        elif c0 > c1:
-            for c in range(c0 - 1, c1, -1):
-                self.build_rail(RAIL_HORIZONTAL, r1, c)
+        # 駅と線路を建設
+        self.first_build(stations)
+        leftovers = []
+        for i in range(2, len(stations)):
+            ok = self.second_build(i, stations)
+            if not ok:
+                leftovers.append(stations[i])
+        for i in range(len(leftovers)):
+            _ = self.second_build(i, leftovers)
 
         # あとは待機
-        while len(self.actions) < self.T:
+        num_stop = len(self.actions)
+        for i in range(num_stop, self.T):
             self.build_nothing()
+
+        return num_stop
 
 
 def main():
     N, M, K, T = map(int, input().split())
 
     data = [tuple(map(int, line.split())) for line in sys.stdin.read().splitlines()]
-    home, work = zip(*[((r0, c0), (r1, c1)) for r0, c0, r1, c1 in data])
+    idx_to_pos = [*[((r0, c0), (r1, c1)) for r0, c0, r1, c1 in data]]
+    pos_to_idx = build_pos_to_idx(idx_to_pos, N)
 
-    solver = Solver(N, M, K, T, home, work)
-    solver.solve()
+    candidate = build_candidate(pos_to_idx, M)
+
+    solver = Solver(N, M, K, T, idx_to_pos, pos_to_idx)
+    stop = solver.solve(candidate)
 
     print("\n".join(map(str, solver.actions)))
-    print(f"score={solver.money}", file=sys.stderr)
+    print(
+        f"score, {solver.money}, stations, {len(solver.stations)}, stop, {stop}",
+        file=sys.stderr,
+    )
 
 
 if __name__ == "__main__":
+    sys.stdin = open("ahc/ahc043.txt")
     main()
